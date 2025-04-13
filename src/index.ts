8781507d169ced920ab40928c13142c6e15ced19
Syed Faraaz Ahmad <faraaz98@live.com>
2025-04-13 06:47:40 +0530

    feat: Parse repo and add files to graph db (#1)
    * feat: add graph db
    
    * fix: repo scanning bugs
    
    * fix: cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,14 @@
 import { createSourceFile, forEachChild, ImportDeclaration, isNamedImports, NamedImports, Node, ScriptTarget, SyntaxKind } from 'typescript';
 import { resolve, dirname } from 'node:path';
 import { globSync, readFileSync } from 'node:fs';
-import { debug } from 'node:console';
+import { executeQuery } from './db';
 
-const REPO_PATH = `${resolve('.')}/rs-admin-api`;
-function print(): void {
-    const fileList = globSync(`${REPO_PATH}/**/**/*.js`)
-
-    let i = 0;
-    for (const file of fileList) {
-        parseFile(file);
-        console.log("\n\n");
-        i += 1;
-        if (i == 10) { return; }
-    }
+interface ImportData {
+    clause: string
+    source: string
 }
 
-const FACILITY_CONTROLLER = `${resolve('.')}/rs-admin-api/src/controllers/facilityAccount.controller.js`;
-
-function parseFile(path: string) {
+function parseFile(path: string, results: ImportData[]): ImportData[] {
     const content = readFileSync(path, 'utf-8');
     const sourceFile = createSourceFile(
         path,
@@ -27,53 +17,103 @@ function parseFile(path: string) {
         true
     );
 
-    traverse(sourceFile);
+    forEachChild(sourceFile, (child) => {
+        const result = traverse(path, child);
+        results = [...results, ...result];
+    })
+
+    return results;
 }
 
-let results: any[] = [];
-function traverse(node: Node, level: number = 0) {
-  if (node.kind === SyntaxKind.ImportDeclaration) {
-      const importDeclaration = node as ImportDeclaration;
-      const importData = {
-          clause: "",
-          source: "",
-      };
-      forEachChild(node, (child) => {
-          if (child.kind === SyntaxKind.StringLiteral) {
-              let path;
-              const text = child.getText();
-              if (text.includes('./')) {
-                  let rawPath = `${dirname(FACILITY_CONTROLLER)}/${text.slice(1, text.length-1)}`;
-                  if (rawPath.includes('./') && !rawPath.endsWith('.js')) { rawPath += '.js'; }
-                  path = resolve(rawPath);
-              } else {
-                  path = text.slice(1, text.length-1);
-              }
+function traverse(filePath: string, node: Node): ImportData[] {
+    if (node.getChildCount() === 0) { return []; }
 
-              importData.source = path;
-          }
-      });
-      if (importDeclaration.importClause?.namedBindings) {
-          const namedBindings = importDeclaration.importClause.namedBindings;
+    let result: ImportData[] = [];
+    if (node.kind === SyntaxKind.ImportDeclaration) {
+        const importDeclaration = node as ImportDeclaration;
+        const importData = {
+            clause: "",
+            source: "",
+        } as ImportData;
 
-        // Check if named bindings are NamedImports
-        if (namedBindings.kind === SyntaxKind.NamedImports) {
-            const namedImports = namedBindings as NamedImports;
+        forEachChild(node, (child) => {
+            if (child.kind === SyntaxKind.StringLiteral) {
+                let path;
+                const text = child.getText();
+                if (text.includes('./')) {
+                    let rawPath = `${dirname(filePath)}/${text.slice(1, text.length - 1)}`;
+                    if (rawPath.includes('./') && !(rawPath.endsWith('.js') || rawPath.endsWith('.json'))) { rawPath += '.js'; }
+                    path = resolve(rawPath);
+                } else {
+                    path = text.slice(1, text.length - 1);
+                }
 
-            // Access each element in NamedImports
-            namedImports.elements.forEach(element => {
-                results.push({ clause: element.name.text, source: importData.source });
-            });
+                importData.source = path;
+            }
+        });
+
+        if (importDeclaration.importClause?.namedBindings) {
+            const namedBindings = importDeclaration.importClause.namedBindings;
+
+            // Check if named bindings are NamedImports
+            if (namedBindings.kind === SyntaxKind.NamedImports) {
+                const namedImports = namedBindings as NamedImports;
+
+                // Access each element in NamedImports
+                namedImports.elements.forEach(element => {
+                    result.push({ clause: element.name.text, source: importData.source });
+                });
+            }
+        } else if (importDeclaration.importClause?.name) {
+            importData.clause = importDeclaration.importClause.name.getText();
+            result.push(importData);
         }
-      } else if (importDeclaration.importClause?.name){
-          importData.clause = importDeclaration.importClause.name.getText();
-          results.push(importData);
-      }
-      return;
-  }
-  forEachChild(node, (child) => traverse(child, level + 1));        // Recursively visit child nodes
+    }
+    return result;
 }
 
-// print();
-parseFile(FACILITY_CONTROLLER);
-console.log(results)
+function registerFile(path: string) {
+    let results: ImportData[] = [];
+    if (!(path[0] === '/')) { return results; }
+
+    results = parseFile(path, []);
+
+    executeQuery(`
+      MERGE (file:File {path: $path})
+      `,
+        { path: path })
+        .catch(err => console.error(err));
+
+    for (const result of results) {
+        executeQuery(`
+          MERGE (file: File {path: $path})
+      `,
+            { path: result.source })
+            .catch(err => console.error(err));
+
+        executeQuery(`
+         match (file1:File { path: $source }), (file2:File { path: $path })
+         merge (file1)-[:IMPORTS_FROM]->(file2)
+      `,
+            { source: path, path: result.source })
+            .catch(err => console.error(err));
+    }
+
+    console.log(`Completed parsing file: ${path}`)
+    return results;
+}
+
+const REPO_PATH = `${resolve('.')}/rs-admin-api`;
+const fileList = globSync(`${REPO_PATH}/**/**/*.js`)
+
+for (const file of fileList) {
+    const results = registerFile(file);
+    for (const result of results) {
+        try {
+            registerFile(result.source);
+        } catch (err) {
+            console.error(err);
+            break;
+        }
+    }
+}
